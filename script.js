8fc415c6fd32b078473599dec22677b7ff1ee9de
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-15 22:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your Script here.
+
+
+document.getElementById('btn').addEventListener('click', (e)=>{
+	let value = document.getElementById('input').value;
+	console.log(value);
+	document.getElementById('p').innerText = ROT13(value);
+})
+
+function ROT13(params) {
+	let result = "";
+	for(let i=0; i<params.length; i++){
+		let charcode = params.charCodeAt(i);
+		if(charcode >=65 && charcode <=90){
+			charcode = ((charcode-65)+13)%26+65;
+		}
+		result += String.fromCharCode(charcode);
+	}
+	return result;
+}
